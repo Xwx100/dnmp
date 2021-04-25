aae8a190bfe361e75e43ceb9930b8d4847ed8974
xuweixin <793486544@qq.com>
2021-04-25 14:18:23 +0800

    resume init

--- www/localhost/index.php
+++ www/localhost/index.php
@@ -24,7 +24,8 @@ function getMysqlVersion()
 {
     if (extension_loaded('PDO_MYSQL')) {
         try {
-            $dbh = new PDO('mysql:host=mysql;dbname=mysql', 'xuweixin', '123456');
+            $dbh = new PDO('mysql:host=mysql;dbname=mysql', 'root', '123456');
+//            $dbh = new PDO('mysql:host=mysql;dbname=mysql', 'xuweixin', '123456');
             $sth = $dbh->query('SELECT VERSION() as version');
             $info = $sth->fetch();
         } catch (PDOException $e) {
